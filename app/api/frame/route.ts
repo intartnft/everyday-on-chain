7d624e1f352e20d570e03c010d8e69a3aa8121aa
0xdude <thedudecrypto@gmail.com>
2024-02-10 22:00:53 +0000

    second image test

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -20,7 +20,8 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         },
       ],
       image: {
-        src: `${NEXT_PUBLIC_URL}/park-2.png`,
+        // src: `${NEXT_PUBLIC_URL}/park-2.png`,
+        src: image,
       },
       ogTitle: name,
       ogDescription: description,
