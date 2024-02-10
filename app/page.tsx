458453c060c7fe04ce91710f0c47a3e2e8862344
0xdude <thedudecrypto@gmail.com>
2024-02-10 22:47:12 +0000

    even more image update

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,43 @@
 import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 import { NEXT_PUBLIC_URL } from './config';
+import { getRandomCollection, parseImageDataURI } from './randomCollection';
 
-const frameMetadata = getFrameMetadata({
-  buttons: [
-    {
-      label: 'Show me random',
+export async function generateMetadata(): Promise<Metadata> {
+  const randomCollection = await getRandomCollection()
+  const image = randomCollection["image"]
+  const fixedImage = parseImageDataURI(image)
+
+  const frameMetadata = getFrameMetadata({
+    buttons: [
+      {
+        label: "Show me random",
+      },
+    ],
+    image: {
+      src: fixedImage,
+      aspectRatio: '1:1'
     },
-  ],
-  image: {
-    src: `${NEXT_PUBLIC_URL}/park-3.png`,
-    aspectRatio: '1:1',
-  },
-  postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-});
+    postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+  });
 
-export const metadata: Metadata = {
-  title: 'everyday on-chain',
-  description: 'LFG',
-  openGraph: {
+  return {
     title: 'everyday on-chain',
     description: 'LFG',
-    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
-  },
-  other: {
-    ...frameMetadata,
-  },
-};
+    openGraph: {
+      title: 'everyday on-chain',
+      description: 'LFG',
+      images: [{
+        url: fixedImage,
+        width: 500,
+        height: 500
+      }],
+    },
+    other: {
+      ...frameMetadata,
+    }
+  }
+}
 
 export default function Page() {
   return (
--- app/randomCollection.ts
+++ app/randomCollection.ts
@@ -4,6 +4,11 @@ const abi = [
     "function tokenURI(uint256) public view returns (string)"
 ]
 
+const collections = [
+    "0x18adc812fe66b9381700c2217f0c9dc816c879e6",
+    "0xca24e7d9e8a2ba3ada22383f5e2ad397b5677e25"
+]
+
 export function parseImageDataURI(uri: string): string {
     if (uri.includes("base64")) {
         return uri
@@ -36,8 +41,11 @@ export async function getRandomCollection(): Promise<any> {
         "function totalSupply() public view returns (uint256)",
         "function tokenURI(uint256) public view returns (string)"
     ]
+
+    const collection = collections[Math.floor(Math.random() * collections.length)]
+    
     const provider = new ethers.JsonRpcProvider("https://eth-mainnet.g.alchemy.com/v2/FFBXRqK9QexBWyV2Z1pFFn-gj_XWKzye", 1);
-    const nftContract = new ethers.Contract("0xca24e7d9e8a2ba3ada22383f5e2ad397b5677e25", abi, provider)
+    const nftContract = new ethers.Contract(collection, abi, provider)
     const totalSupplyBig = await nftContract.totalSupply()
     const totalSupply = parseInt(totalSupplyBig)
 
