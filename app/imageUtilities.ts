345a98db9ab47635cc739a4f5cd6de555389cc0b
0xdude <thedudecrypto@gmail.com>
2024-02-11 14:37:49 +0000

    another update

--- app/imageUtilities.ts
+++ app/imageUtilities.ts
@@ -9,10 +9,12 @@ export async function parseImageDataURI(uri: string): Promise<string> {
     const imageData = uri.split('utf8,')[1];
     const base64 = Buffer.from(imageData).toString('base64');
     return 'data:image/svg+xml;base64,' + base64;
-  }else if (uri.includes('http')) {
-    const base64 = await encode(uri, {string: true})
-    return 'data:image/image;base64,' + base64
   }
+
+//   else if (uri.includes('http')) {
+//     const base64 = await encode(uri, {string: true})
+//     return 'data:image/image;base64,' + base64
+//   }
 
   return uri;
 }
