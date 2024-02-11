d775e899cc883c55ed9b9beb6a8d1f1ee4f9752b
0xdude <thedudecrypto@gmail.com>
2024-02-11 00:48:52 +0000

    another aspect ratio update

--- app/svgWrapper.ts
+++ app/svgWrapper.ts
@@ -1,9 +1,16 @@
 
 export function wrapImageSource(source: string, width: number, height: number) {
     const maxSize = Math.max(width, height)
+    let offsetX = 0
+    let offsetY = 0
+    if (width > height) {
+        // offsetY = (width - height) * 0.5
+    }else{
+        // offsetX = (height - width) * 0.5
+    }
     const lines = [
-        '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xhtml="http://www.w3.org/1999/xhtml" viewBox="0 0 ' + maxSize + ' ' + maxSize + '" x="0" y="0" width="' + maxSize + '" height="' + maxSize + '">',
-        '<foreignObject x="0" y="0" width="' + width + '" height="' + height + '">',
+        '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xhtml="http://www.w3.org/1999/xhtml" viewBox="0 0 ' + width + ' ' + height + '" x="0" y="0" width="100%" height="100%">',
+        '<foreignObject x="' + offsetX + '" y="' + offsetY + '" width="' + width + '" height="' + height + '">',
         '<xhtml:img style="width:100%;height:100%" src="' + source + '"/>',
         '</foreignObject></svg>'
     ]
